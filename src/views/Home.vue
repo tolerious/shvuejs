c82a415ea6c87a01df054bdab548cf4468080066
tolerious feng <tolerious@qq.com>
2019-11-08 00:09:49 +0800

    :lipstick: content make-up.
    fill content on homepage.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="@/assets/vue-logo.png" alt="vue logo.">
+    <h1>Welcome to the VueJS Shanghai Meetup!
+    </h1>
+    <h2>Vue is coming...</h2>
+    <a href="https://vuejs.org/v2/guide/">Get Started with Vue</a>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    components: {
+    }
   }
-}
 </script>
+
+<style lang="less">
+  .home {
+    text-align: center;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1,
+  h2 {
+    color: white;
+    margin-bottom: 60px;
+  }
+
+  h1 {
+    font-weight: 700;
+  }
+
+  h2 {
+    font-weight: 400;
+
+  }
+
+  img {
+    width: 300px;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    background: linear-gradient(-200deg, #ff2a30, #8c0a1d);
+    padding: 20px;
+    border-radius: 5px;
+    font-weight: 700
+  }
+</style>
